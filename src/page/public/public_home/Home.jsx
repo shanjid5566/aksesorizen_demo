d2d458d4e966af2d51549866535464a8b57b3e66
shanjid5566 <shanjidahmed66@gmail.com>
2025-12-31 13:05:05 +0600

    add pricing section

--- src/page/public/public_home/Home.jsx
+++ src/page/public/public_home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './components/Banner'
 import Features from './components/Features'
+import Pricing from './components/Pricing'
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Features />
+      <Pricing />
     </div>
   )
 }
